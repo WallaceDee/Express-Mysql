d596d769f456734b49d741f8704a55136476bb01
WallaceDee <447363121@qq.com>
2019-08-12 17:36:07 +0800

    更新

--- config/db.js
+++ config/db.js
@@ -1,24 +1,8 @@
-var mysql = require('mysql');
-var dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'x',
-    port: 3306,
-    multipleStatements: true
-}
-var pool = mysql.createPool(dbConfig);
 module.exports = {
-    executeSql: function(sql, param, callback) {
-        pool.getConnection(function(error, connection) {
-            if (error) {
-                console.log(error)
-            } else {
-                connection.query(sql, param, function(err, result) {
-                    callback(err, result);
-                });
-            }
-            pool.releaseConnection(connection);
-        });
-    }
-};
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "x",
+    port: 3306,
+    multipleStatements: true//运行多行查询
+  };
