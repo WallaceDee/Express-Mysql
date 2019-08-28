09246726ef58fe0af664621c7f2d4ee3ad91cbc9
WallaceDee <447363121@qq.com>
2019-08-28 14:50:56 +0800

    更新sql

--- x.sql
+++ x.sql
@@ -1,48 +1,124 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : h
-Source Server Version : 50703
-Source Host           : localhost:3306
+Source Server         : x
+Source Server Version : 80013
+Source Host           : www.chenguohua.com.cn:3306
 Source Database       : x
 
 Target Server Type    : MYSQL
-Target Server Version : 50703
+Target Server Version : 80013
 File Encoding         : 65001
 
-Date: 2017-11-08 16:26:14
+Date: 2019-08-28 14:49:29
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `table_user`
+-- Table structure for pal_item_category
+-- ----------------------------
+DROP TABLE IF EXISTS `pal_item_category`;
+CREATE TABLE `pal_item_category` (
+  `categoryId` int(11) NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `parentId` int(11) DEFAULT '0',
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for table_advertisement
+-- ----------------------------
+DROP TABLE IF EXISTS `table_advertisement`;
+CREATE TABLE `table_advertisement` (
+  `adId` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '',
+  `cover` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '',
+  `url` varchar(255) DEFAULT '',
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`adId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for table_article
+-- ----------------------------
+DROP TABLE IF EXISTS `table_article`;
+CREATE TABLE `table_article` (
+  `articleId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` longtext,
+  `authorUserId` varchar(20) NOT NULL,
+  `categoryId` varchar(20) NOT NULL,
+  `updateTime` varchar(255) NOT NULL,
+  `createTime` varchar(255) NOT NULL,
+  PRIMARY KEY (`articleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for table_blog
+-- ----------------------------
+DROP TABLE IF EXISTS `table_blog`;
+CREATE TABLE `table_blog` (
+  `blogId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `type` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `url` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `authorUserId` bigint(255) DEFAULT NULL,
+  `categoryId` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `createTime` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `updateTime` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`blogId`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Table structure for table_category
+-- ----------------------------
+DROP TABLE IF EXISTS `table_category`;
+CREATE TABLE `table_category` (
+  `categoryId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parentId` bigint(20) NOT NULL DEFAULT '0',
+  `categoryName` varchar(255) NOT NULL,
+  `createTime` varchar(255) DEFAULT NULL,
+  `updateTime` varchar(255) DEFAULT NULL,
+  `sortIndex` bigint(255) DEFAULT '0',
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Table structure for table_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `table_comment`;
+CREATE TABLE `table_comment` (
+  `commentId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  `parentId` bigint(20) DEFAULT '0',
+  `authorId` bigint(20) DEFAULT NULL,
+  `articleId` bigint(20) DEFAULT NULL,
+  `createTime` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `authorNickName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`commentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=142 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for table_user
 -- ----------------------------
 DROP TABLE IF EXISTS `table_user`;
 CREATE TABLE `table_user` (
   `userId` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userOpenId` varchar(255) DEFAULT NULL,
-  `userName` varchar(255) NOT NULL DEFAULT '游客',
-  `userPassword` varchar(16) NOT NULL,
-  `userNickName` varchar(255) DEFAULT NULL,
-  `userAvatar` varchar(255) DEFAULT NULL,
-  `userBirthday` varchar(255) DEFAULT NULL,
-  `userPhone` varchar(255) DEFAULT NULL,
-  `userGender` int(1) unsigned zerofill DEFAULT '0',
+  `qqOpenId` varchar(255) DEFAULT NULL,
+  `weiboId` varchar(255) DEFAULT NULL,
+  `userName` varchar(255) DEFAULT NULL,
+  `userPassword` varchar(255) DEFAULT NULL,
+  `userNickName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `userAvatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `userGender` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'u',
+  `job` varchar(255) DEFAULT '',
+  `signature` varchar(255) DEFAULT '',
+  `userBirthday` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  `userPhone` varchar(255) DEFAULT '',
   `createTime` varchar(255) DEFAULT NULL,
   `updateTime` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of table_user
--- ----------------------------
-INSERT INTO `table_user` VALUES ('28', null, 'admin', 'admin', null, '/upload/user_avatar/ad9f8ea0-b56c-11e7-a2f9-9992c1811d66.png', null, '13570437475', '1', '2017-11-20 11:04:11', '2017-11-08 15:53:59');
-INSERT INTO `table_user` VALUES ('30', null, 'test', '1', null, '/upload/user_avatar/ad9f8ea0-b56c-11e7-a2f9-9992c1811d66.png', null, '', '1', '2017-10-19 17:36:24', '2017-10-30 10:04:58');
-INSERT INTO `table_user` VALUES ('32', null, '123', '123', '陈国华', 'upload/user_avatar/f0f22130-c44f-11e7-9404-db8324cab383.png', '2017-11-04', '12345678901', '0', '2017-10-23 15:50:04', '2017-11-08 16:24:46');
-INSERT INTO `table_user` VALUES ('36', null, '234', '23423', null, '/upload/user_avatar/ad9f8ea0-b56c-11e7-a2f9-9992c1811d66.png', null, '1', '0', '2017-10-23 16:07:47', '2017-10-23 16:07:47');
-INSERT INTO `table_user` VALUES ('37', null, '123123', '213', null, '/upload/user_avatar/ad9f8ea0-b56c-11e7-a2f9-9992c1811d66.png', null, '1', '0', '2017-10-23 16:13:31', '2017-10-23 16:13:31');
-INSERT INTO `table_user` VALUES ('38', null, '333', '11122', null, null, null, null, '0', '2017-11-08 11:42:33', '2017-11-08 11:42:33');
-INSERT INTO `table_user` VALUES ('39', null, '123', '123', '陈国华', 'upload/user_avatar/f0f22130-c44f-11e7-9404-db8324cab383.png', '2017-11-04', null, '0', '2017-11-08 11:43:10', '2017-11-08 16:24:46');
-INSERT INTO `table_user` VALUES ('40', null, '1994', '123', null, null, null, null, '0', '2017-11-08 11:52:12', '2017-11-08 13:28:17');
-INSERT INTO `table_user` VALUES ('41', null, 'qwe', 'qwe', '葛銊扄搈', 'upload/user_avatar/fb6ded90-c451-11e7-a7ed-a5fc491bebc2.png', '2017-11-05 00:00:00', '13202627449', '0', '2017-11-08 14:55:57', '2017-11-08 15:43:25');
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
